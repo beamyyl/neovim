c00be7246e3dfa6ad9000a1dd2271e9ed9be3936
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-06 17:42:18 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 vim.opt.termguicolors = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
 require("tokyonight").setup({
   style = "night",
   transparent = false, 
-  terminal_colors = true,
+ terminal_colors = true,
   styles = {
     italic = {
         comments = true,
