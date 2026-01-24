fc48325fa2ccb2f776dca7a5988cb66e0eabbc31
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-25 01:15:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 mkdir -p ~/.config/nvim && \
 mkdir -p ~/.local/share/nvim/site/pack/tokyonight/start && \
 git clone --filter=blob:none https://github.com ~/.local/share/nvim/site/pack/tokyonight/start/tokyonight.nvim && \
-git clone https://github.com && \
 cp -r neovim/* ~/.config/nvim/ && \
 rm -rf neovim && \
-git clone https://github.com ~/.local/share/nvim/site/pack/plugins/start/nvim-highlight-colors
+git clone https://github.com/brenoprata10/nvim-highlight-colors ~/.local/share/nvim/site/pack/plugins/start/nvim-highlight-colors
